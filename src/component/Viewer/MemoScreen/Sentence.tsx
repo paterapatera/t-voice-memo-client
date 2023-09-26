45652f58f077458530954e870689c217b6407388
namataka <pater4.1127+patera@gmail.com>
2023-09-26 20:00:09 +0900

    正規表現のバグ修正

--- src/component/Viewer/MemoScreen/Sentence.tsx
+++ src/component/Viewer/MemoScreen/Sentence.tsx
@@ -3,7 +3,11 @@ import Markdown from 'markdown-to-jsx'
 
 export function Sentence({ children, onClick }: { children: string, onClick: () => void }) {
     // eslint-disable-next-line no-useless-escape
-    return <Paper onClick={onClick}><Markdown>{children.replace(/^( *(\d+\. {1,4}|[\w\<\'\">\-*+])[^\n]*)\n{1}(?!\n| *\d+\. {1,4}| *[-*+] +|$)/gm, function (text: string) {
+    return <Paper onClick={onClick}><Markdown>{children.replace(/^[\p{L}<>'"][^\n]*\n{1}(?!\n| *\d+\. {1,4}| *[-*+] +|$)/gmu, function (text: string) {
+        console.log(text)
         return text.trim() + "  \n"
+    }).replace(/^( *(\d+\. {1,4}|[*+-])[^\n]*)\n{1}(?!\n| *\d+\. {1,4}| *[-*+] +|$)/gmu, function (text: string) {
+        console.log(text)
+        return text.trim() + "\n\n"
     })}</Markdown></Paper>
 }
